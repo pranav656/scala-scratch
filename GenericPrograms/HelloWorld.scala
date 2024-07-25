dc6f20f3429dd79e4f707526e911272d2365a80b
Pranav Sailesh Mani <pranav656@gmail.com>
2024-07-25 09:22:54 +0200

    Create HelloWorld.scala

---
+++ GenericPrograms/HelloWorld.scala
@@ -0,0 +1,5 @@
+// hello is a method defined with def. It is
+// declared to be main with main annotation. 
+// It prints the hello world string to standard 
+// output using println method
+@main def hello() = println("Hello, World!")
